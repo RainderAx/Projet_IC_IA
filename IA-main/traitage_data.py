8f99064b42d5f80a4d7dbaed91c564fcc481f2b0
RainderAx <114318268+RainderAx@users.noreply.github.com>
2024-02-18 23:34:02 +0100

    correction csv

--- IA-main/traitage_data.py
+++ IA-main/traitage_data.py
@@ -4,20 +4,15 @@ import csv
 with open('data_dist.json', 'r') as f:
     data_dist = json.load(f)
 
-
 sensor_data = data_dist['sensor']
 
-with open('output.csv', 'w', newline='') as f:
+with open('donee.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Distance', 'Value'])
 
-     writer = csv.writer(f)
-
-     writer.writerow(['Distance', 'Value'])
-
-     for key, value in sensor_data['us'].items():
-        # key est l'identifiant unique, value est un dictionnaire contenant les données de chaque capteur
+    for key, value in sensor_data['us'].items():
         distance = value.get('Distance')
-        time = value.get('Time')
         sensor_value = value.get('value')
-        
-        writer.writerow([distance, value])
+        writer.writerow([distance, sensor_value])
+
 
